e514f2cb220aa645f9e4428a0e7821b38d11593e
Melo-dy <melodydiarra@gmail.com>
2020-10-07 17:00:19 +0200

    update avatar

--- src/components/App/Footer/AboutUs/index.js
+++ src/components/App/Footer/AboutUs/index.js
@@ -6,7 +6,7 @@ import avatar from '../AboutUs/avatar.jpeg';
 const AboutUs = () => {
     return(
         <div className="about">
-            <h1>Qui sommes-nous ?</h1>
+            <h2>Qui sommes-nous ?</h2>
             
             <div className="about-elements">
                 <div className="about-elt1">
@@ -16,22 +16,29 @@ const AboutUs = () => {
                 </div>
 
                 <div className="about-elt2">
-                    <h4>Sarah</h4>
-                    <img className="about-avatar"src={avatar} alt=""/>
-                    <p>Scrum Master</p>
-                    <h4>Jérémy</h4>
-                    <img className="about-avatar"src={avatar}alt=""/>
-                    <p>Lead Dev Back</p>
+                    <div className="about-scrum">
+                        <h4>Sarah</h4>
+                        <img className="about-avatar"src={avatar} alt=""/>
+                        <p>Scrum Master</p>
+                    </div>
+                    <div className="about-back">
+                        <h4>Jérémy</h4>
+                        <img className="about-avatar"src={avatar}alt=""/>
+                        <p>Lead Dev Back</p>
+                    </div>
                 </div>
                     
                 <div className="about-elt3">
-                    <h4>Aurélie</h4>
-                    <img className="about-avatar"src={avatar}alt=""/>
-                    <p>Lead Dev Front</p>
-
+                    <div className="about-front">
+                        <h4>Aurélie</h4>
+                        <img className="about-avatar"src={avatar}alt=""/>
+                        <p>Lead Dev Front</p>
+                    </div>
+                    <div className="about-master">
                     <h4>Melo</h4>
                     <img className="about-avatar"src={avatar}alt=""/>
                     <p>Git master</p>
+                    </div>
                 </div>
             </div>
         </div>
