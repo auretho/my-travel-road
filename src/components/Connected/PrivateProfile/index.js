3eb758c1b87ef1569e91d0ee9cb01c0f54e629e5
karastras <burrus.jerome.oc@gmail.com>
2020-10-12 17:57:36 +0200

    fixed

--- src/components/Connected/PrivateProfile/index.js
+++ src/components/Connected/PrivateProfile/index.js
@@ -11,7 +11,6 @@ import './styles.scss';
 const privateProfile = ({ userData }) => {
     console.log(userData)
     return (
-<<<<<<< HEAD
         <div className="privateProfile">
             
             <div className="privateProfile-infos">
@@ -45,39 +44,6 @@ const privateProfile = ({ userData }) => {
                 <div className="privateProfile-buttons">
                     <Link to='/edit-profile' >
                         <button className="privateProfile-button">Editer le profil</button>
-=======
-        <div className="consult">
-            <div className="consult-cover">
-                <img className="consult-content" src={userData.cover} alt="" />
-        </div>
-            
-             <div className="consult-profil">
-                <h6><b>Nom</b></h6>
-                <p>{userData.lastname}</p>
-                
-                <h6><b>Prénom</b></h6>
-                <p>{userData.firstname}</p>
-                
-                <h6><b>Email</b></h6>
-                <p>{userData.email}</p>
-                
-                <h6><b>Ville</b></h6>
-                <p>{userData.city}</p>
-                
-                <h6><b>Code Postal</b></h6>
-                <p>{userData.postal}</p>
-                <Link to='/edit-profile' >
-                    <button className="consult-button">Editer le profil</button>
-                </Link>
-             </div>
-            
-            <div className="consult-pseudo">
-                {/*<button className="consult-public">Profil public</button>*/}
-                    <h2 className="consult-name">{userData.nickname}<span>&#x2661;</span></h2>
-                    <img className="consult-avat"src={userData.avatar} alt=""/>
-                    <Link to='/my-travels' >
-                        <button className="consult-pseudo button">Mes voyages</button>
->>>>>>> 9e3190f080fa7f331a05527696d0b7f7c5f43bf5
                     </Link>
                     <Link to='/public-profile' >
                         <button className="privateProfile-button">Ma vue publique</button>
@@ -88,7 +54,6 @@ const privateProfile = ({ userData }) => {
                 </div>
 
             </div>
-<<<<<<< HEAD
 
             <div className="privateProfile-images">
 
@@ -103,16 +68,6 @@ const privateProfile = ({ userData }) => {
                 </div>
 
                 
-=======
-            
-            <div className="consult-article">
-                <article>
-                    <h4>A Propos</h4>
-                        <p>
-                            {userData.presentation}
-                        </p>
-                </article>
->>>>>>> 9e3190f080fa7f331a05527696d0b7f7c5f43bf5
             </div>
         </div>
 
