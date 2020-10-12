736fdad6cff601ab5aeac3c81fbab1cbeb5f34df
Melo-dy <melodydiarra@gmail.com>
2020-10-12 17:26:45 +0200

    add resp

--- src/components/App/Footer/Contact/index.js
+++ src/components/App/Footer/Contact/index.js
@@ -9,27 +9,27 @@ const Contact = () => {
         <div className="contact">
             <div className="contact-intro">
                 <h1>Contactez-nous</h1>
-                <p>Merci de remplir vos informations et nous vous répondrons dans les plus brefs délais.</p>
+                <p className="contact-info">Merci de remplir vos informations et nous vous répondrons dans les plus brefs délais.</p>
             </div>
             
             <div className="contact-form">
                 <form>
-                    <label for="name">Nom </label>
+                    <label className="contact-label" for="name">Nom </label>
                         <input className="contact-input" type="text" /><br/>
                     
-                    <label for="name">Prénom </label>
+                    <label className="contact-label" for="name">Prénom </label>
                         <input className="contact-input" type="text" /><br/>
                     
-                    <label for="name">Email</label>
+                    <label className="contact-label" for="name">Email</label>
                         <input className="contact-input" type="text" /><br/>
                     
-                    <label for="name">Téléphone* </label>
+                    <label className="contact-label" for="name">Téléphone* </label>
                         <input className="contact-input" type="number" /><br/>
                     
-                    <label for="name">Sujet du message*</label>
+                    <label className="contact-label" for="name">Sujet du message*</label>
                         <input className="contact-input" type="text" /><br/>
                     
-                    <label for="name">Message*</label>
+                    <label className="contact-label" for="name">Message*</label>
                         <textarea className="contact-input" type="text" rows="7"></textarea><br/>
                     
                     <button className="contact-button"type="submit">Envoyer</button>
